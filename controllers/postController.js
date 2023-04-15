30ce27078a26036586398f751d307a2181d51f0e
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-04-15 13:08:50 -0400

    added get posts/post and remove post route and controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,4 +25,37 @@ const createPost = async (req, res) => {
   }
 };
 
-module.exports = { createPost };
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({date: -1});
+    res.json(posts);
+  } catch(e) {
+    console.error(e);
+    res.status(500).json('Internal server error');
+  }
+};
+
+const getOnePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if(!post) return res.status(404).json({msg: 'Post Not Found'});
+    res.json(post);
+  } catch(e) {
+    console.error(e);
+    if(e.kind === 'ObjectId') return res.status(404).json({msg: 'Post Not Found'});
+    res.status(500).json('Internal server error');
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const response = await Post.findOneAndDelete({_id: req.params.post_id, user: req.user.id});
+    if (!response) return res.status(404).json('Post Not Found');
+    res.json(response);
+  } catch(e) {
+    console.error(e);
+    res.status(500).json('Internal server error');
+  }
+}
+
+module.exports = { createPost, getAllPosts, getOnePost, deletePost };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
-const { createPost } = require('../../controllers/postController');
+const { createPost, getAllPosts, getOnePost, deletePost } = require('../../controllers/postController');
 
-// @route GET api/posts
-// @desc Test route
-// @access Public
+// create post
 router.post('/', [auth, [check('text', 'text is required').not().isEmpty()]], createPost);
+
+// get all posts
+router.get('/', auth, getAllPosts);
+
+// get one post by id
+router.get('/:post_id', auth, getOnePost);
+
+// delete post by id
+router.delete('/:post_id', auth, deletePost);
 
 module.exports = router;
