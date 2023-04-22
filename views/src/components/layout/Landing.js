ad32483903e075b9772df14bea32d3b5dd56724e
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-04-22 19:36:55 -0400

    added login form in client side

--- views/src/components/layout/Landing.js
+++ views/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Landing() {
   return (
@@ -7,12 +8,12 @@ function Landing() {
         <div className="landing-inner">
           <h1 className="large">Developers Connect Here</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts and get help from
+            Create a developer profile, share posts and get help from
             other developers
           </p>
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign Up</a>
-            <a href="login.html" className="btn btn-light">Login</a>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-light">Login</Link>
           </div>
         </div>
       </div>
